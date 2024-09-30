07b643792bceebb9872253e658f9f92a2f551695
madmediaz2 <simar.cakmak@gmail.com>
2024-09-30 19:23:31 +0200

    removed unused imports

--- src/app/app/server-info/page.tsx
+++ src/app/app/server-info/page.tsx
@@ -1,7 +1,5 @@
 // pages/about.tsx
 import React from 'react';
-import ParticlesBackground from '@components/Particles';
-import styles from './page.module.scss'
 
 const InfoPage: React.FC = () => {
   return (
