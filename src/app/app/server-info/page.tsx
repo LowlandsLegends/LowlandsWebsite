edfdcaa887396e84c3bbd2accedff449631e13d5
madmediaz2 <simar.cakmak@gmail.com>
2024-10-03 23:31:56 +0200

    styling and layout

--- src/app/app/server-info/page.tsx
+++ src/app/app/server-info/page.tsx
@@ -26,6 +26,16 @@ const InfoPage: React.FC = () => {
 					playerCount={0}
 				/>
 			</div>
+			<div className={styles.cardWrapper}>
+				<ServerCard
+					imageSrc='/images/Ark_SE.svg'
+					href='/'
+					title='[EU/NL] ASA NEDERLAND SCORCHED EARTH'
+					ipAdress='127.0.0.1:80	'
+					gameMode='PVP'
+					playerCount={0}
+				/>
+			</div>
 		</div>
 	);
 };
