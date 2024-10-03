3af847d09f9a96470ad50c744511d7bf252a7ed2
madmediaz2 <simar.cakmak@gmail.com>
2024-10-03 23:19:11 +0200

    Fixed flexboxes.

--- src/app/app/server-info/page.tsx
+++ src/app/app/server-info/page.tsx
@@ -1,17 +1,32 @@
 // pages/about.tsx
 import React from 'react';
 import ServerCard from '@components/ServerCard'
+import styles from './page.module.scss'
 
 const InfoPage: React.FC = () => {
 	return (
-		<ServerCard
-			imageSrc='/images/Logo.svg'
-			href='/'
-			title='[EU/NL] ASA NEDERLAND SCORCHED EARTH'
-			ipAdress='127.0.0.1:80'
-			gameMode='PVP'
-			playerCount={0}
-		/>
+		<div className={styles.main}>
+			<div className={styles.cardWrapper}>
+				<ServerCard
+					imageSrc='/images/Ark_SE.svg'
+					href='/'
+					title='[EU/NL] ASA NEDERLAND SCORCHED EARTH'
+					ipAdress='127.0.0.1:80	'
+					gameMode='PVP'
+					playerCount={0}
+				/>
+			</div>
+			<div className={styles.cardWrapper}>
+				<ServerCard
+					imageSrc='/images/Ark_SE.svg'
+					href='/'
+					title='[EU/NL] ASA NEDERLAND SCORCHED EARTH'
+					ipAdress='127.0.0.1:80	'
+					gameMode='PVP'
+					playerCount={0}
+				/>
+			</div>
+		</div>
 	);
 };
 
