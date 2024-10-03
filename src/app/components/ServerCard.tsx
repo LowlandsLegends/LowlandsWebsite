ceadd396afa63c3430445a4fa2ffd46365d9c4b3
madmediaz2 <simar.cakmak@gmail.com>
2024-10-03 23:19:01 +0200

    Server Card styling

--- src/app/components/ServerCard.tsx
+++ src/app/components/ServerCard.tsx
@@ -15,18 +15,9 @@ interface ServerCardProps {
 
 export default function ServerCard({title, href, ipAdress, imageSrc, gameMode, playerCount }: ServerCardProps) {
     return (
-        <div style={{ width: 1, height: 1 }} className={styles.cardWrapper}>
+        <div className={styles.cardWrapper} style={{display:'inline-block'}}>
             <a href={href}>
                 <div className={styles.card}>
-                    <ParticlesBackground
-                        imageSrc={imageSrc}
-                        imageSize={[5, 10]}
-                        density={[100, 150]}
-                        click={false}
-                        speed={4}
-                        link={false}
-                        hover={false}
-                    />
                     <div className={styles.titleWrapper}>
                         <span className={styles.text}>
                             {title}
@@ -41,15 +32,17 @@ export default function ServerCard({title, href, ipAdress, imageSrc, gameMode, p
                         />
                     </div>
                     <div className={styles.footer}>
-                        <span className={styles.text}>
-                            {ipAdress}
-                        </span>
-                        <span className={styles.text}>
-                            {gameMode}
-                        </span>
-                        <span className={styles.text}>
-                            Online: {playerCount}
-                        </span>
+                        <div className={styles.spanWrapper}>
+                            <span className={styles.text}>
+                                {ipAdress}
+                            </span>
+                            <span className={styles.text}>
+                                {gameMode}
+                            </span>
+                            <span className={styles.text}>
+                                Online: {playerCount}
+                            </span>
+                        </div>
                     </div>
                 </div>
             </a>
