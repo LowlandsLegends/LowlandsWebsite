4d64f466609cb5b72158af0a6ff96ccbafe0ef56
madmediaz2 <simar.cakmak@gmail.com>
2024-10-07 23:14:09 +0200

    Added Particlesbackground to /app route

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from '@/components/navbar';
-
+import ParticlesBackground from "@components/Particles"
 
 export default function RootLayout({
   children,
@@ -8,6 +8,15 @@ export default function RootLayout({
 }) {
   return (
     <div style={{ height: '100%', width: '100%' }}>
+      <ParticlesBackground
+        imageSrc='/images/logo.svg'
+        imageSize={[7, 10]}
+        density={[15, 15]}
+        click={false}
+        speed={4}
+        link={true}
+        hover={false}
+      />
 		<Navbar />
         {children}
     </div>
