4b0970763488257200fc64e79d392b61b5d93b8d
madmediaz2 <simar.cakmak@gmail.com>
2024-10-07 23:14:36 +0200

    Added boxshadow theme extend in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+		boxShadow: {
+			custom: "0 2px 20px rgba(0, 0, 0, 0.35)",
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -58,6 +61,7 @@ module.exports = {
   				'5': 'hsl(var(--chart-5))'
   			}
   		}
+		
   	}
   },
   plugins: ["tailwindcss-animate"],
