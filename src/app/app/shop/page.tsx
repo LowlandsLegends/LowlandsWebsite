6d448f44e3884ead183f4761fc533cce388ba582
madmediaz2 <simar.cakmak@gmail.com>
2024-10-08 16:55:54 +0200

    Added categoryselect for the shop page

--- src/app/app/shop/page.tsx
+++ src/app/app/shop/page.tsx
@@ -1,15 +1,29 @@
+'use client';
 import React from "react";
-import CheckoutButton from "@components/CheckoutButton";
+import { CategorySelectorComponent } from "@/components/category-selector";
+import styles from './page.module.scss'
+
+
+const categories = [
+    { id: "1", name: "Starter kits", image: "/images/ascended_tier.jpg" },
+    { id: "2", name: "Dino's", image: "/images/ascended_tier.jpg" },
+    { id: "3", name: "Blueprints", image: "/images/ascended_tier.jpg" },
+    { id: "4", name: "Rebuild kits", image: "/images/ascended_tier.jpg" },
+    { id: "5", name: "Boss kits", image: "/images/ascended_tier.jpg" },
+    { id: "6", name: "Lootboxes", image: "/images/ascended_tier.jpg" }
+]
+
 
 const ShopPage:React.FC = () => {
-    const items = [
-        { name: 'Product 1', price: 1000, quantity: 1 }, // price in cents
-        { name: 'Product 2', price: 2000, quantity: 2 },
-    ];
+    const handleCategorySelect = (categoryId: string) => {
+        console.log("Selected category:", categoryId)
+        // Add your logic here to update the displayed products based on the selected category
+    }
     return(
-        
-        <div>
-            <CheckoutButton items={items}></CheckoutButton>
+        <div style={{height:'90%', display:'flex'}}>
+            <div className={styles.main} style={{marginLeft:"10px"}}>
+                <CategorySelectorComponent categories={categories} onSelect={handleCategorySelect} />
+            </div>
         </div>
     );
 }
