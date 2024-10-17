9a9fd6bb93f1afa24e6d5909012de75d12f6693b
madmediaz2 <simar.cakmak@gmail.com>
2024-10-17 22:28:01 +0200

    updated formatting

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -2,23 +2,23 @@ import { Navbar } from '@/components/navbar';
 import ParticlesBackground from "@components/Particles"
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <ParticlesBackground
-        imageSrc='/images/logo.svg'
-        imageSize={[7, 10]}
-        density={[15, 15]}
-        click={false}
-        speed={4}
-        link={true}
-        hover={false}
-      />
-		<Navbar />
-        {children}
-    </div>
-  )
+	return (
+		<div style={{ height: '100%', width: '100%' }}>
+			<ParticlesBackground
+				imageSrc='/images/logo.svg'
+				imageSize={[7, 10]}
+				density={[15, 15]}
+				click={false}
+				speed={4}
+				link={true}
+				hover={false}
+			/>
+			<Navbar />
+			{children}
+		</div>
+	)
 }
