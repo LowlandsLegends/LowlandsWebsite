499d54ac8701ff27a6d987a3a1f3f5bbec387c71
madmediaz2 <simar.cakmak@gmail.com>
2024-11-04 17:09:42 +0100

    Added all servercards to server-info route

--- src/app/app/server-info/page.tsx
+++ src/app/app/server-info/page.tsx
@@ -1,31 +1,63 @@
 // pages/about.tsx
 import React from 'react';
-import ServerCard from '@components/ServerCard'
 import styles from './page.module.scss'
 import { RCONScheduler } from '@lib/rcon';
+import ServerInfoCard from '../../components/ServerInfoCard';
 
 const InfoPage: React.FC = () => {
 
-	const islandPlayers = async () => {
-		const island = new RCONScheduler(0)
-		await island.connectRCON();
-		const players = await island.getPlayerCount()
+	const getPlayers = async (index:number): Promise<number> => {
+		const obj = new RCONScheduler(index);
+		await obj.connectRCON();
+		const players = await obj.getPlayerCount()
 		return players
 	}
 
+	const islandPlayers = async () => { return await getPlayers(0); }
+	const centerPlayers = async () => { return await getPlayers(1); }
+	const scorchedEarthPlayers = async () => { return await getPlayers(2); }
+	const aberrationPlayers = async () => { return await getPlayers(3); }
+
 	return (
 		<div className={styles.main}>
-			<div className={styles.cardWrapper}>
-				<ServerCard
-					imageSrc='/images/Ark_SE.svg'
-					href='/'
-					title='[EU/NL] ASA NEDERLAND SCORCHED EARTH'
-					ipAdress='127.0.0.1:80	'
-					gameMode='PVP'
-					playerCount={islandPlayers()}
-				/>
-			</div>
+			<ServerInfoCard
+				serverName='[EU/NL] ASA NEDERLAND Abberation'
+				serverIp='127.0.0.1:80'
+				gamemode='pvp'
+				isOnline={true}
+				playerCount={aberrationPlayers()}
+				upTime={99}
+				serverIndex={3}
+			/>
+			<ServerInfoCard 
+				serverName='[EU/NL] ASA NEDERLAND island'
+				serverIp='127.0.0.1:80'
+				gamemode='pvp'
+				isOnline={true}
+				playerCount={islandPlayers()}
+				upTime={99}
+				serverIndex={0}
+			/>
+			<ServerInfoCard 
+				serverName='[EU/NL] ASA NEDERLAND center'
+				serverIp='127.0.0.1:80'
+				gamemode='pvp'
+				isOnline={true}
+				playerCount={centerPlayers()}
+				upTime={99}
+				serverIndex={1}
+			/>
+			<ServerInfoCard 
+				serverName='[EU/NL] ASA NEDERLAND scorchedEarth'
+				serverIp='127.0.0.1:80'
+				gamemode='pvp'
+				isOnline={true}
+				playerCount={scorchedEarthPlayers()}
+				upTime={99}
+				serverIndex={2}
+			/>
 		</div>
+		
 	);
 };
 
