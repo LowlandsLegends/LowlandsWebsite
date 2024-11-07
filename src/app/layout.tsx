7dfb303040e543a354cf367e7c237777c3262825
madmediaz2 <simar.cakmak@gmail.com>
2024-11-07 01:46:10 +0100

    removed playerchecks from top-level layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,67 +1,6 @@
 import '@styles/globals.css';
 import '@styles/globals.scss';
 
-import { RCONScheduler } from '@lib/rcon';
-
-async function fetchAndStoreIslandPlayerCount(){
-	const scheduler = new RCONScheduler(0);
-	try {
-		await scheduler.connectRCON();
-		const playerCount = await scheduler.getPlayerCount();
-		await scheduler.storePlayerCount(playerCount);
-		await scheduler.rconClient?.end();
-	} catch (error) {
-		console.error(`Error fetching player count for server ${scheduler.index}:`, error);
-	}
-}
-
-async function fetchAndStoreCenterPlayerCount(){
-	const scheduler = new RCONScheduler(1);
-	try {
-		await scheduler.connectRCON();
-		const playerCount = await scheduler.getPlayerCount();
-		await scheduler.storePlayerCount(playerCount);
-		await scheduler.rconClient?.end();
-	} catch (error) {
-		console.error(`Error fetching player count for server ${scheduler.index}:`, error);
-	}
-}
-
-async function fetchAndStoreScorchedEarthPlayerCount(){
-	const scheduler = new RCONScheduler(2);
-	try {
-		await scheduler.connectRCON();
-		const playerCount = await scheduler.getPlayerCount();
-		await scheduler.storePlayerCount(playerCount);
-		await scheduler.rconClient?.end();
-	} catch (error) {
-		console.error(`Error fetching player count for server ${scheduler.index}:`, error);
-	}
-}
-
-async function fetchAndStoreAberrationPlayerCount(){
-	const scheduler = new RCONScheduler(3);
-	try {
-		await scheduler.connectRCON();
-		const playerCount = await scheduler.getPlayerCount();
-		await scheduler.storePlayerCount(playerCount);
-		await scheduler.rconClient?.end();
-	} catch (error) {
-		console.error(`Error fetching player count for server ${scheduler.index}:`, error);
-	}
-}
-
-setInterval(fetchAndStoreAberrationPlayerCount, 360000);
-fetchAndStoreAberrationPlayerCount();
-
-setInterval(fetchAndStoreIslandPlayerCount, 360000);
-fetchAndStoreIslandPlayerCount();
-
-setInterval(fetchAndStoreCenterPlayerCount, 360000);
-fetchAndStoreCenterPlayerCount();
-
-setInterval(fetchAndStoreScorchedEarthPlayerCount, 360000);
-fetchAndStoreScorchedEarthPlayerCount();
 
 export default function RootLayout({
 	children,
