969410ae98d8e59faa564d0a796cc26c53d113c3
madmediaz2 <simar.cakmak@gmail.com>
2024-11-08 17:43:01 +0100

    using SWR to reduce ISR reads on vercel

--- src/app/components/ServerInfoCard.tsx
+++ src/app/components/ServerInfoCard.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Users, Swords, Shield } from "lucide-react"
 import LineChartComponent from "./LineChart"
 import Graph from '@images/Graph.svg'
-import { useEffect, useState } from "react"
+import useSWR from 'swr'
 
 interface ServerInfoProps {
 	serverName: string
@@ -19,11 +19,16 @@ export interface ChartDataPoint {
 	playerCount: number;
 }
 
-const StatusIndicator = ({ isOnline, isLoading }: { isOnline: boolean | Promise<boolean>; isLoading: boolean }) => (
+const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+const StatusIndicator = ({ isOnline, isLoading }: { isOnline: boolean | unknown; isLoading: boolean }) => (
 	<div
-		className={`w-3 h-3 rounded-full ${
-			isLoading ? 'bg-orange-500' : isOnline ? 'bg-green-500' : 'bg-red-900'
-		}`}
+		className={`w-3 h-3 rounded-full ${isLoading || typeof isOnline !== 'boolean'
+				? 'bg-orange-500'
+				: isOnline
+					? 'bg-green-500'
+					: 'bg-red-900'
+			}`}
 	/>
 )
 
@@ -34,45 +39,29 @@ export default function ServerInfoCard({
 	upTime = 99,
 	serverIndex,
 }: ServerInfoProps) {
-	const [data, setData] = useState<ChartDataPoint[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [isOnline, setIsOnline] = useState(false);
-
-	useEffect(() => {
-		async function fetchData() {
-			try {
-				const response = await fetch(`/api/player-count/${serverIndex}`);
-				const result = await response.json();
-				setData(result);
-				setIsLoading(false);
-				if (response.status === 503) {
-					setIsOnline(false);
-					return;
-				} else {
-					setIsOnline(true);
-				}
-			} catch (error) {
-				console.error('Error fetching player count data:', error);
-			}
+	const { data, error, isValidating } = useSWR<ChartDataPoint[]>(
+		`/api/player-count/${serverIndex}`,
+		fetcher,
+		{
+			refreshInterval: 300000, // 5 minutes in milliseconds
+			revalidateOnFocus: false, // Prevent revalidation on focus
 		}
+	)
 
-		fetchData();
+	const isOnline = !error && data && data.length > 0
 
-		/** const interval = setInterval(fetchData, 60000);
 
-		return () => clearInterval(interval); **/
-	}, [serverIndex]);
 	return (
 		<Card className="w-full max-w-md overflow-hidden p-1">
 			<CardHeader className="pb-0">
 				<div className="flex items-center justify-between">
 					<CardTitle className="text-xl font-bold">{serverName}</CardTitle>
-					<StatusIndicator isOnline={isOnline} isLoading={isLoading} />
+					<StatusIndicator isOnline={isOnline!} isLoading={isValidating} />
 				</div>
 			</CardHeader>
 			<CardContent className="pt-4 h-fit">
 				<div className="aspect-video w-full mb-3 overflow-hidden rounded-md h-[150px]">
-					<LineChartComponent serverIndex={serverIndex} data={data} isLoading={isLoading} />
+					<LineChartComponent serverIndex={serverIndex} data={data || []} isLoading={isValidating} />
 				</div>
 				<div className="grid grid-cols-2 gap-4 mr-0 pb-0 mb-0 justify-center items-center">
 					<div className="flex items-center text-white">
@@ -92,7 +81,7 @@ export default function ServerInfoCard({
 					</div>
 					<div className="flex items-center text-white">
 						<Users className="w-5 h-5 mr-1 text-white" />
-						{Array.isArray(data) && data.length > 0 ? data[data.length - 1].playerCount : 0} players online
+						{data && data.length > 0 ? data[data.length - 1].playerCount : 0} players online
 					</div>
 					<div className="flex items-center text-white">
 						<div className="mr-1 text-white pl-5">Uptime: </div>
