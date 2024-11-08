9e0daf4c7f6f622ddcd0f4168ccdd011f6d890ab
madmediaz2 <simar.cakmak@gmail.com>
2024-11-08 18:54:42 +0100

    Icon Stying is now done with tailwindCSS

--- src/app/components/Icon.tsx
+++ src/app/components/Icon.tsx
@@ -1,7 +1,6 @@
 // components/Icon.tsx
 
 import React from 'react';
-import styles from './Icon.module.scss';
 
 interface IconProps {
 	Component: React.FC<React.SVGProps<SVGSVGElement>>;
@@ -11,8 +10,31 @@ interface IconProps {
 
 const Icon: React.FC<IconProps> = ({ Component, alt, className = '', ...props }) => {
 	return (
-		<div className={`${styles.iconContainer} ${className}`} aria-label={alt}>
-			<Component className={styles.icon} {...props} />
+		<div
+			className={`flex items-center justify-center w-[100px] h-[100px] ${className}`}
+			aria-label={alt}
+		>
+			<Component
+				className={`
+          relative
+          w-[clamp(65px,6.5vw,130px)]
+          h-[65px]
+          transition-transform duration-300 ease
+          rounded-[30%] 
+          shadow-[0_2px_20px_rgba(0,0,0,0.35)]
+          backdrop-blur-[5px]
+          p-2.5
+          hover:scale-110
+          md:w-[clamp(110px,13vw,220px)]
+          md:h-[100px]
+          md:rounded-[30%]
+          md:shadow-[0_2px_20px_rgba(0,0,0,0.35)]
+          md:backdrop-blur-[5px]
+          md:px-2.5
+          md:py-2.5
+        `}
+				{...props}
+			/>
 		</div>
 	);
 };
