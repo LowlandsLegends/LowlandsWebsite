9020804a2edb0ca42e6b8cc598ef208fac26d6da
madmediaz2 <simar.cakmak@gmail.com>
2024-11-10 14:37:21 +0100

    Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@styles/globals.css';
 import '@styles/globals.scss';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function RootLayout({
@@ -12,6 +13,7 @@ export default function RootLayout({
 			<body style={{ height: '100vh', margin: 0 }}>
 				<div style={{ height: '100%', width: '100%' }}>
 					{children}
+					<Analytics />
 				</div>
 			</body>
 		</html>
