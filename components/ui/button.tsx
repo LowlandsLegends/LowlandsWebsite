794918372f21da3e6e495fd1d441a8b0d8e64cca
madmediaz2 <simar.cakmak@gmail.com>
2024-11-11 21:56:32 +0100

    Added button variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,54 +5,60 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-white/10 hover:bg-white/20 text-white backdrop-blur-md border-0 rounded-full px-6 py-3",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/40 rounded-full",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-        navbar: 'bg-white/5 hover:bg-white/15 text-dark backdrop-blur-md border-1 shadow-lg rounded-[30%]',
-        shoppingcart: 'rounded-lg bg-card-dark text-card-foreground shadow-custom backdrop-blur-sm'
-      },
-      size: {
-        default: "",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  }
+	"inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+	{
+		variants: {
+			variant: {
+				default:
+					"bg-white/10 hover:bg-white/20 text-white backdrop-blur-md border-0 rounded-full px-6 py-3",
+				destructive:
+					"bg-destructive text-destructive-foreground hover:bg-destructive/40 rounded-full",
+				outline:
+					"border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+				secondary:
+					"bg-secondary text-secondary-foreground hover:bg-secondary/80",
+				ghost: "hover:bg-accent hover:text-accent-foreground",
+				link: "text-primary underline-offset-4 hover:underline",
+				navbar:
+					"bg-white/5 hover:bg-white/15 text-dark backdrop-blur-md border-1 shadow-lg rounded-[30%]",
+				shoppingcart:
+					"rounded-lg bg-card-dark text-card-foreground shadow-custom backdrop-blur-sm",
+				login:
+					"h-9 w-max rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+				loginmobile:
+					"inline-flex items-center justify-start text-left bg-background px-4 py-2 rounded-md", // New Variant
+			},
+			size: {
+				default: "",
+				sm: "h-9 rounded-md px-3",
+				lg: "h-11 rounded-md px-8",
+				icon: "h-10 w-10",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+			size: "default",
+		},
+	}
 )
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+	VariantProps<typeof buttonVariants> {
+	asChild?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
+	({ className, variant, size, asChild = false, ...props }, ref) => {
+		const Comp = asChild ? Slot : "button"
+		return (
+			<Comp
+				className={cn(buttonVariants({ variant, size, className }))}
+				ref={ref}
+				{...props}
+			/>
+		)
+	}
 )
 Button.displayName = "Button"
 
